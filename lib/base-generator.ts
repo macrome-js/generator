df7ae254fffc7c698bdb7dafc4e48ae404831ba4
Conrad Buck <conartist6@gmail.com>
2022-02-09 11:29:33 -0500

    include and exclude types on BaseGenerator

--- lib/base-generator.ts
+++ lib/base-generator.ts
@@ -1,6 +1,8 @@
-import { Generator } from "./types";
+import { Generator, MMatchExpression } from "./types";
 
 export class BaseGenerator<O, T> implements Generator<O, T> {
+  include?: MMatchExpression;
+  exclude?: MMatchExpression;
   options: O;
 
   constructor(options: O) {
